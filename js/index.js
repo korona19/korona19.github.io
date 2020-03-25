dd27ba700f7f05542a66ea19ab5d064e4bec2eb3
korona19 <62501058+korona19@users.noreply.github.com>
2020-03-25 12:06:49 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -127,7 +127,7 @@ function DisplayGraph() {
 	var graph_data = CreateGraphData();
 	var layout = {
 		title: 'COVID 19 Daily Data',
-		height:400,
+		height:750,
 		plot_bgcolor:"#e9ecef",
 		paper_bgcolor:"#e9ecef",
 		xaxis: {
